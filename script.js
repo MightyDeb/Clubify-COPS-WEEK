b43abfeed9c98a7a32e5791fdc201de3b36bcc87
MightyDeb <debapriyamaity2005@gmail.com>
2024-04-15 15:37:12 +0530

    changes

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const faqs=document.querySelectorAll(".question_answer");
 faqs.forEach(function(element){
   element.addEventListener('click',function(e){
     const data= e.currentTarget;
+    let x=data.innerHTML;
+    let result = x.includes("add");
+    if(result){
+      const arr=x.split("add");
+      data.innerHTML=arr[0]+"remove"+arr[1];
+    }else{
+      const arr=x.split("remove");
+      data.innerHTML=arr[0]+"add"+arr[1];
+    }
     data.classList.toggle("show-text");
   })
 }
